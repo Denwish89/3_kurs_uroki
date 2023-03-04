b01e2b277f2c11b736d54147ee9ad70d694758d0
denis <denwish@list.ru>
2023-03-04 21:07:24 +0300

    add task 5 & 7

---
+++ Task07/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на входе принимает
+трехзначное число, а на выходе показывает последнюю цифру этого числа
+456 -> 6
+782 -> 2
+918 -> 8 */
+
+Console.Write("Введите трехзначное натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number > 99 && number < 1000)
+{
+    int total = number % 10;
+    Console.WriteLine(total); 
+}
+else
+{
+    Console.WriteLine("Нужно ввести трехзначное натурально число");
+}
