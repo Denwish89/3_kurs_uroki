aaa4fe5f89c5fc8f2b4e3bd4c8323bb2f9ab25f8
denis <denwish@list.ru>
2023-03-28 15:50:41 +0300

    add Task42

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,4 +3,18 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.Write("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string binareCode = BinareCode(number);
+Console.WriteLine(binareCode);
 
+string BinareCode(int num)
+{
+    string result = string.Empty;
+    while(num > 0)
+    {
+        result += num % 2;
+        num /= 2;
+    }
+    return result;
+}
